45fc224cbf1a4d029ffaa96f1cedef6ba6a8ce51
durcyn <durcyn@gmail.com>
2012-09-04 02:44:04 -0500

    5.0: add Mounts, lose LFR

--- picoButtons.lua
+++ picoButtons.lua
@@ -1,18 +1,15 @@
-
-local IHASFOURTWO = select(4, GetBuildInfo()) >= 40200
-
 local ldb = LibStub:GetLibrary("LibDataBroker-1.1")
 local microcoord = {0, 1, 22/64, 1}
 
 
 local i = 0
-local function helper(title, button, label, icon, coords, click)
+local function helper(title, label, click, icon, coords)
 	ldb:NewDataObject(i.. " MicroButton "..title, {
 		type = "launcher",
 		icon = icon or "Interface\\Buttons\\UI-MicroButton-"..title.."-Up",
 		texcoord = coords or (not icon and microcoord),
 		iconCoords = coords or (not icon and microcoord),
-		OnClick = click or button:GetScript("OnClick"),
+		OnClick = click, 
 		label = label,
 	})
 	i = i + 1
@@ -22,22 +19,18 @@ end
 local x = MainMenuMicroButton:GetScript("OnMouseUp")
 local function mainmenu(self, ...) self.down = 1; x(self, ...) end
 
-helper("Character", CharacterMicroButton, CHARACTER_BUTTON, "Interface\\GroupFrame\\UI-Group-LeaderIcon", nil, function() ToggleCharacter("PaperDollFrame") end)
-helper("Spellbook", SpellbookMicroButton, SPELLBOOK_ABILITIES_BUTTON)
-helper("Talents", TalentMicroButton, TALENTS_BUTTON)
-helper("Achievement", AchievementMicroButton, ACHIEVEMENT_BUTTON)
-helper("Quest", QuestLogMicroButton, QUESTLOG_BUTTON)
-helper("Guild", GuildMicroButton, GUILD, "Interface\\Buttons\\UI-MicroButton-Socials-Up", microcoord)
-helper("PVP", PVPMicroButton, PLAYER_V_PLAYER, "Interface\\TargetingFrame\\UI-PVP-"..UnitFactionGroup("player"), {0, 5/8, 0, 5/8}, TogglePVPFrame)
-helper("LFD", LFDMicroButton, DUNGEONS_BUTTON, "Interface\\Buttons\\UI-MicroButton-LFG-Up", microcoord)
-
-if IHASFOURTWO then
-	helper("EJ", EJMicroButton, ENCOUNTER_JOURNAL)
-	helper("Raid", RaidMicroButton, RAID)
-end
-
-helper("MainMenu", MainMenuMicroButton, MAINMENU_BUTTON, nil, nil, mainmenu)
-helper("Help", HelpMicroButton, HELP_BUTTON)
+helper("Character", CHARACTER_BUTTON, function() ToggleCharacter("PaperDollFrame") end, "Interface\\GroupFrame\\UI-Group-LeaderIcon")
+helper("Spellbook", SPELLBOOK_ABILITIES_BUTTON, function() ToggleSpellBook(BOOKTYPE_SPELL) end)
+helper("Talents", TALENTS_BUTTON, function() ToggleTalentFrame() end)
+helper("Achievement", ACHIEVEMENT_BUTTON, function() ToggleAchievementFrame() end)
+helper("Quest", QUESTLOG_BUTTON, function() ToggleFrame(QuestLogFrame) end)
+helper("Guild", GUILD, ToggleGuildFrame, "Interface\\Buttons\\UI-MicroButton-Socials-Up", microcoord)
+helper("PVP", PLAYER_V_PLAYER, TogglePVPFrame, "Interface\\TargetingFrame\\UI-PVP-"..UnitFactionGroup("player"), {0, 5/8, 0, 5/8})
+helper("LFD", DUNGEONS_BUTTON, function() PVEFrame_ToggleFrame() end, "Interface\\Buttons\\UI-MicroButton-LFG-Up", microcoord)
+helper("Mounts", MOUNTS_AND_PETS, TogglePetJournal)
+helper("EJ", ENCOUNTER_JOURNAL, ToggleEncounterJournal)
+helper("MainMenu", MAINMENU_BUTTON, mainmenu)
+helper("Help", HELP_BUTTON, ToggleHelpFrame)
 
 
 ---------------------------
